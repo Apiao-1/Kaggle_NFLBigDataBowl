9b09d7d5e6c84fa42586c30e4b2c6b8e5b446f2d
Apiao <ms_zhangpuping@163.com>
2019-10-22 15:15:12 +0800

    init

---
+++ readme.md
@@ -0,0 +1,56 @@
+# EDA
+
+### Data
+
+https://www.kaggle.com/c/nfl-big-data-bowl-2020/data
+
+- `GameId` - a unique game identifier 比赛ID
+- `PlayId` - a unique play identifier 一场比赛中所有play的ID（每次play都有可能得分）
+- `Team` - home or away 队名
+- `X` - player position along the long axis of the field. See figure below.
+- `Y` - player position along the short axis of the field. See figure below.
+ ![img](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F3258%2F7542d363a19fa3eea77708e6b90bc420%2FFig1.png?generation=1570562067917019&alt=media)
+> NFL和NCAA使用的标准球场是一个长360英尺（120码或109.7米）、宽160英尺（53.33码或48.8米）的长方形草坪（有些室内赛会使用仿草地毯），较长的边界称为边线（sideline），较短的边界称为端线（end line）。端线前的标示线称为得分线（goal line），球场每侧端线与得分线之间有一个纵深10码（9.1米）的得分区叫做端区（end zone，也称达阵区），端区的四角各有一个约有1英尺长的橙色长方体标柱（pylon）。两侧得分线相距100码（91.44米），之间的区域也就是比赛区（playing field）。比赛区上距离得分线每5码（4.6米）距离标划一条码线（yard line，或称5码线），每10码标示数字，直到50码线到达中场（midfield）。在球场中间和两侧与边线平行排列划有横向的短标示线，称为码标（hash marks，或称整码线），其中接近边线的码标线称为界内线（inbounds line）。任何球员都必须在码标线上或之间进行发球。
+- `S` - speed in yards/second 此时的速度
+- `A` - acceleration in yards/second^2 加速度
+- `Dis` - distance traveled from prior time point, in yards
+- `Orientation` - orientation of player (deg) 运动员此时的朝向
+- `Dir` - angle of player motion (deg) 运动的方向
+- `NflId` - a unique identifier of the player 运动员ID
+- `DisplayName` - player's name 运动员name
+- `JerseyNumber` - jersey number 运动员号码
+- `Season` - year of the season
+- `YardLine` - the yard line of the line of scrimmage 发球的码线
+- `Quarter` - game quarter (1-5, 5 == overtime) 比赛所处的时间
+- `GameClock` - time on the game clock 
+- `PossessionTeam` - team with possession
+- `Down` - the down (1-4)
+- `Distance` - yards needed for a first down
+- `FieldPosition` - which side of the field the play is happening on
+- `HomeScoreBeforePlay` - home team score before play started 主队已经获得的比分
+- `VisitorScoreBeforePlay` - visitor team score before play started 客队已经获得的比分
+- `NflIdRusher` - the `NflId` of the rushing player
+- `OffenseFormation` - offense formation
+- `OffensePersonnel` - offensive team positional grouping
+- `DefendersInTheBox` - number of defenders lined up near the line of scrimmage, spanning the width of the offensive line
+- `DefensePersonnel` - defensive team positional grouping
+- `PlayDirection` - direction the play is headed
+- `TimeHandoff` - UTC time of the handoff
+- `TimeSnap` - UTC time of the snap
+- **`Yards` - the yardage gained on the play (you are predicting this)** 得分
+- `PlayerHeight` - player height (ft-in)
+- `PlayerWeight` - player weight (lbs)
+- `PlayerBirthDate` - birth date (mm/dd/yyyy)
+- `PlayerCollegeName` - where the player attended college
+- `HomeTeamAbbr` - home team abbreviation
+- `VisitorTeamAbbr` - visitor team abbreviation
+- `Week` - week into the season
+- `Stadium` - stadium where the game is being played
+- `Location` - city where the game is being player
+- `StadiumType` - description of the stadium environment 体育馆类型
+- `Turf` - description of the field surface 场地类型
+- `GameWeather` - description of the game weather
+- `Temperature` - temperature (deg F)
+- `Humidity` - humidity 湿度
+- `WindSpeed` - wind speed in miles/hour
+- `WindDirection` - wind direction
---
+++ test/test.py
@@ -0,0 +1,195 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import datetime
+# from kaggle.competitions import nflrush
+import tqdm
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import keras
+
+from tqdm import tqdm_notebook
+import warnings
+
+warnings.filterwarnings('ignore')
+
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_rows', 200)
+pd.set_option('display.max_columns', 200)
+sns.set_style('darkgrid')
+mpl.rcParams['figure.figsize'] = [15, 10]
+
+
+# https://www.kaggle.com/rooshroosh/fork-of-neural-networks-different-architecture
+def strtoseconds(txt):
+    txt = txt.split(':')
+    ans = int(txt[0]) * 60 + int(txt[1]) + int(txt[2]) / 60
+    return ans
+
+
+def strtofloat(x):
+    try:
+        return float(x)
+    except:
+        return -1
+
+
+def map_weather(txt):
+    ans = 1
+    if pd.isna(txt):
+        return 0
+    if 'partly' in txt:
+        ans *= 0.5
+    if 'climate controlled' in txt or 'indoor' in txt:
+        return ans * 3
+    if 'sunny' in txt or 'sun' in txt:
+        return ans * 2
+    if 'clear' in txt:
+        return ans
+    if 'cloudy' in txt:
+        return -ans
+    if 'rain' in txt or 'rainy' in txt:
+        return -2 * ans
+    if 'snow' in txt:
+        return -3 * ans
+    return 0
+
+
+def OffensePersonnelSplit(x):
+    dic = {'DB': 0, 'DL': 0, 'LB': 0, 'OL': 0, 'QB': 0, 'RB': 0, 'TE': 0, 'WR': 0}
+    for xx in x.split(","):
+        xxs = xx.split(" ")
+        dic[xxs[-1]] = int(xxs[-2])
+    return dic
+
+
+def DefensePersonnelSplit(x):
+    dic = {'DB': 0, 'DL': 0, 'LB': 0, 'OL': 0}
+    for xx in x.split(","):
+        xxs = xx.split(" ")
+        dic[xxs[-1]] = int(xxs[-2])
+    return dic
+
+
+def orientation_to_cat(x):
+    x = np.clip(x, 0, 360 - 1)
+    try:
+        return str(int(x / 15))
+    except:
+        return "nan"
+
+
+def preprocess(train):
+    ## GameClock
+    train['GameClock_sec'] = train['GameClock'].apply(strtoseconds)
+    train["GameClock_minute"] = train["GameClock"].apply(lambda x: x.split(":")[0]).astype("object")
+
+    ## Height
+    train['PlayerHeight_dense'] = train['PlayerHeight'].apply(
+        lambda x: 12 * int(x.split('-')[0]) + int(x.split('-')[1]))
+
+    ## Time
+    train['TimeHandoff'] = train['TimeHandoff'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
+    train['TimeSnap'] = train['TimeSnap'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
+
+    train['TimeDelta'] = train.apply(lambda row: (row['TimeHandoff'] - row['TimeSnap']).total_seconds(), axis=1)
+    train['PlayerBirthDate'] = train['PlayerBirthDate'].apply(lambda x: datetime.datetime.strptime(x, "%m/%d/%Y"))
+
+    ## Age
+    seconds_in_year = 60 * 60 * 24 * 365.25
+    train['PlayerAge'] = train.apply(
+        lambda row: (row['TimeHandoff'] - row['PlayerBirthDate']).total_seconds() / seconds_in_year, axis=1)
+    train["PlayerAge_ob"] = train['PlayerAge'].astype(np.int).astype("object")
+
+    ## WindSpeed
+    train['WindSpeed_ob'] = train['WindSpeed'].apply(
+        lambda x: x.lower().replace('mph', '').strip() if not pd.isna(x) else x)
+    train['WindSpeed_ob'] = train['WindSpeed_ob'].apply(
+        lambda x: (int(x.split('-')[0]) + int(x.split('-')[1])) / 2 if not pd.isna(x) and '-' in x else x)
+    train['WindSpeed_ob'] = train['WindSpeed_ob'].apply(
+        lambda x: (int(x.split()[0]) + int(x.split()[-1])) / 2 if not pd.isna(x) and type(
+            x) != float and 'gusts up to' in x else x)
+    train['WindSpeed_dense'] = train['WindSpeed_ob'].apply(strtofloat)
+
+    ## Weather
+    train['GameWeather_process'] = train['GameWeather'].str.lower()
+    train['GameWeather_process'] = train['GameWeather_process'].apply(
+        lambda x: "indoor" if not pd.isna(x) and "indoor" in x else x)
+    train['GameWeather_process'] = train['GameWeather_process'].apply(
+        lambda x: x.replace('coudy', 'cloudy').replace('clouidy', 'cloudy').replace('party', 'partly') if not pd.isna(
+            x) else x)
+    train['GameWeather_process'] = train['GameWeather_process'].apply(
+        lambda x: x.replace('clear and sunny', 'sunny and clear') if not pd.isna(x) else x)
+    train['GameWeather_process'] = train['GameWeather_process'].apply(
+        lambda x: x.replace('skies', '').replace("mostly", "").strip() if not pd.isna(x) else x)
+    train['GameWeather_dense'] = train['GameWeather_process'].apply(map_weather)
+
+    ## Rusher
+    train['IsRusher'] = (train['NflId'] == train['NflIdRusher'])
+    train['IsRusher_ob'] = (train['NflId'] == train['NflIdRusher']).astype("object")
+    temp = train[train["IsRusher"]][["Team", "PlayId"]].rename(columns={"Team": "RusherTeam"})
+    train = train.merge(temp, on="PlayId")
+    train["IsRusherTeam"] = train["Team"] == train["RusherTeam"]
+
+    ## dense -> categorical
+    train["Quarter_ob"] = train["Quarter"].astype("object")
+    train["Down_ob"] = train["Down"].astype("object")
+    train["JerseyNumber_ob"] = train["JerseyNumber"].astype("object")
+    train["YardLine_ob"] = train["YardLine"].astype("object")
+    # train["DefendersInTheBox_ob"] = train["DefendersInTheBox"].astype("object")
+    # train["Week_ob"] = train["Week"].astype("object")
+    # train["TimeDelta_ob"] = train["TimeDelta"].astype("object")
+
+    ## Orientation and Dir
+    train["Orientation_ob"] = train["Orientation"].apply(lambda x: orientation_to_cat(x)).astype("object")
+    train["Dir_ob"] = train["Dir"].apply(lambda x: orientation_to_cat(x)).astype("object")
+
+    train["Orientation_sin"] = train["Orientation"].apply(lambda x: np.sin(x / 360 * 2 * np.pi))
+    train["Orientation_cos"] = train["Orientation"].apply(lambda x: np.cos(x / 360 * 2 * np.pi))
+    train["Dir_sin"] = train["Dir"].apply(lambda x: np.sin(x / 360 * 2 * np.pi))
+    train["Dir_cos"] = train["Dir"].apply(lambda x: np.cos(x / 360 * 2 * np.pi))
+
+    ## diff Score
+    train["diffScoreBeforePlay"] = train["HomeScoreBeforePlay"] - train["VisitorScoreBeforePlay"]
+    train["diffScoreBeforePlay_binary_ob"] = (train["HomeScoreBeforePlay"] > train["VisitorScoreBeforePlay"]).astype(
+        "object")
+
+    ## Turf
+    Turf = {'Field Turf': 'Artificial', 'A-Turf Titan': 'Artificial', 'Grass': 'Natural',
+            'UBU Sports Speed S5-M': 'Artificial', 'Artificial': 'Artificial', 'DD GrassMaster': 'Artificial',
+            'Natural Grass': 'Natural', 'UBU Speed Series-S5-M': 'Artificial', 'FieldTurf': 'Artificial',
+            'FieldTurf 360': 'Artificial', 'Natural grass': 'Natural', 'grass': 'Natural', 'Natural': 'Natural',
+            'Artifical': 'Artificial', 'FieldTurf360': 'Artificial', 'Naturall Grass': 'Natural',
+            'Field turf': 'Artificial', 'SISGrass': 'Artificial', 'Twenty-Four/Seven Turf': 'Artificial',
+            'natural grass': 'Natural'}
+    train['Turf'] = train['Turf'].map(Turf)
+
+    ## OffensePersonnel
+    temp = train["OffensePersonnel"].iloc[np.arange(0, len(train), 22)].apply(
+        lambda x: pd.Series(OffensePersonnelSplit(x)))
+    temp.columns = ["Offense" + c for c in temp.columns]
+    temp["PlayId"] = train["PlayId"].iloc[np.arange(0, len(train), 22)]
+    train = train.merge(temp, on="PlayId")
+
+    ## DefensePersonnel
+    temp = train["DefensePersonnel"].iloc[np.arange(0, len(train), 22)].apply(
+        lambda x: pd.Series(DefensePersonnelSplit(x)))
+    temp.columns = ["Defense" + c for c in temp.columns]
+    temp["PlayId"] = train["PlayId"].iloc[np.arange(0, len(train), 22)]
+    train = train.merge(temp, on="PlayId")
+
+    ## sort
+    #     train = train.sort_values(by = ['X']).sort_values(by = ['Dis']).sort_values(by=['PlayId', 'Team', 'IsRusher']).reset_index(drop = True)
+    train = train.sort_values(by=['X']).sort_values(by=['Dis']).sort_values(
+        by=['PlayId', 'IsRusherTeam', 'IsRusher']).reset_index(drop=True)
+    return train
+
+
+if __name__ == '__main__':
+    # train = pd.read_csv('../input/nfl-big-data-bowl-2020/train.csv', dtype={'WindSpeed': 'object'})
+    train = pd.read_csv('../data/train.csv', dtype={'WindSpeed': 'object'})
+    # train = train[:2200]
+    # print(train.shape)# (509762, 49)
+    # print(train.head())
